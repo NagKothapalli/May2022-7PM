d3efec84d82a714f8289ba598d31ee7dc2f633de
NagKothapalli <nag022@gmail.com>
2022-05-19 10:21:54 +0530

    May18th code pushed

--- src/simpleInheritance/TestCases.java
+++ src/simpleInheritance/TestCases.java
@@ -1,9 +1,13 @@
-package basicConcepts;
+package simpleInheritance;
 
 import org.junit.Test;
-
-public class GmailWithJunit
+//             Child               Parent   -> Simple Inheritance
+public class TestCases extends ReusableComponents
 {
+	/*
+	 * public void launchApplication() {
+	 * System.out.println("RC : Launch Application from TC"); }
+	 */
 	//Junit : Unit Test Framework . We can run test methods with the help of junit
 	//A Java function will be treated as Test Method / Test Case  after adding @Test annotation
 	@Test   //TestCase - Step1 , Step2 , Step3 , Step4 ........
@@ -50,54 +54,6 @@ public class GmailWithJunit
 		delete();
 		logoutFromApplication();
 		closeApplication();
-	}
-	//********************Reusable Components ********************
-	void launchApplication()
-	{
-		System.out.println("RC : Launch Application");
-		//launch
-		//dfdfdfdfd
-		//gghfhgh
-		//fghgfhj
-		//tytr5656
-		//ffdg78768
-		//fggh34324
-	}
-	private void loginToApplication()
-	{
-		System.out.println("RC : Login Application");
-	}
-	public void logoutFromApplication()
-	{
-		System.out.println("RC : Logout From Application");
-	}
-	private void closeApplication()
-	{
-		System.out.println("RC : Close Application");
-	}
-	public void compose()
-	{
-		System.out.println("RC : Compose Mail");
-	}
-	public void send()
-	{
-		System.out.println("RC : Send Mail");		
-	}
-	void open()
-	{
-		System.out.println("RC : Open Mail");
-	}
-	public void reply()
-	{
-		System.out.println("RC : Reply Mail");
-	}
-	protected void forward()
-	{
-		System.out.println("RC : Forward Mail");
-	}
-	public void delete()
-	{
-		System.out.println("RC : Delete Mail");
 	}
 
 }
